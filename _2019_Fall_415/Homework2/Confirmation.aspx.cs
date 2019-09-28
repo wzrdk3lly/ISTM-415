77701dca0871912f2140a3038f7c41ef5ec5fadf
milespc <dkmk1149@gmail.com>
2019-09-28 13:40:39 -0500

    Revert "transfers text box info to second sheet"
    This reverts commit a9fb615d7db1659064354f1cf10acc9697f91ead.

--- _2019_Fall_415/Homework2/Confirmation.aspx.cs
+++ _2019_Fall_415/Homework2/Confirmation.aspx.cs
@@ -9,18 +9,6 @@ public partial class Homework2_Confirmation : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-        TextBox previousFirstName = (TextBox)PreviousPage.FindControl("txtFirstName");
-        lblFirstName.Text = previousFirstName.Text;
-        TextBox previousLastName = (TextBox)PreviousPage.FindControl("txtLastName");
-        TextBox previousEmail = (TextBox)PreviousPage.FindControl("txtEmailAddress");
-        TextBox previousTelephone = (TextBox)PreviousPage.FindControl("txtTelephoneNumber");
-        DropDownList previousPrefered = (DropDownList)PreviousPage.FindControl("ddlPreffereedMethod");
-
-        TextBox previousArrival = (TextBox)PreviousPage.FindControl("txtArrivalDate");
-        TextBox previousDeparture = (TextBox)PreviousPage.FindControl("txtDepartureDate");
-    
-        DropDownList previousNumberPeople = (DropDownList)PreviousPage.FindControl("ddlNumberPeople");
-        RadioButtonList previousBedType = (RadioButtonList)PreviousPage.FindControl("rblBedType");
-        TextBox previousSpeialRequest = (TextBox)PreviousPage.FindControl("txtSpecialRequest");
+       
     }
 }
